dac1f089ba4483134ed6b025c478b23a1d35f5ad
ritesh-cfc <riteshdashcfc1236@gmail.com>
2018-01-06 17:41:59 +0530

    Add files via upload

---
+++ rectangle.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define lli long long int 
+using namespace std;
+
+
+int main(){
+	int t=0;
+	cin>>t;
+	while(t--){
+		lli arr[4];
+	for(int i=0;i<4;i++)
+	cin>>arr[i];
+	sort(arr,arr+4);
+	if(arr[0]==arr[1]&&arr[2]==arr[3])
+	cout<<"YES"<<endl;
+	else
+	cout<<"NO"<<endl;
+	}
+	return 0;
+}
